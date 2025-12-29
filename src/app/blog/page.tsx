3b5db83af3c06d1be260fdcc1645cf30b406814f
juyalrohit <rohitjuyalp205@gmail.com>
2025-12-29 10:51:08 +0530

    Make responsive

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,9 @@
-"use client";
 
-import { useState } from "react";
 import Navbar from '@/components/Home/Navbar';
 import Footer from '@/components/Home/Footer';
 import Content from '@/components/blog/Content';
 import LetsConnect from "@/components/blog/LetsConnect";
+
 
 
 const page = () => {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
                   <Partners />
                   <Banner /> 
                   <Trending />
-                   <Courses /> 
+                   {/* <Courses />  */}
                   <Enquiry /> 
                   <TopCourses />
                   
